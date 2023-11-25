88059f8f46f88c0f51033d16b1863102ee9b084f
Adam <wangadam019@gmail.com>
2023-11-25 12:03:44 -0500

    incorporated mongodb

--- index.js
+++ index.js
@@ -1,64 +1,66 @@
-//importing libraries
-const cors = require("cors");
 const express = require("express");
-const session = require("express-session");
-require("dotenv").config();
-const { Configuration, OpenAIApi } = require("openai");
-const twilio = require("twilio");
 const bodyParser = require("body-parser");
+const { MongoClient } = require('mongodb');
+const cors = require("cors");
+require("dotenv").config();
 
-//initializing app + defining port
 const app = express();
 const port = 3001;
 
-//connecting middlewear
 app.use(bodyParser.json());
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(
-  session({
-    secret: "dispatchBot",
-    resave: false,
-    saveUninitialized: true,
-    cookie: {},
-  })
-);
+
+const uri = "mongodb+srv://wangadam019:123@cluster0.csarsun.mongodb.net/?retryWrites=true&w=majority"; // Replace with your MongoDB URI
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+app.post("/", async (req, res) => {
+  try {
+    const postData = req.body;
+    console.log("Received POST request:", postData);
+
+    // Connect to MongoDB
+    await client.connect();
+
+    const database = client.db('rescue'); // Replace with your database name
+    const collection = database.collection('person'); // Replace with your collection name
+
+    // Insert the data into the collection
+    await collection.insertOne(postData);
+
+    // Send a response back to the client
+    res.send("POST request received and data inserted successfully");
+  } catch (error) {
+    console.error('Error handling POST request:', error);
+    res.status(500).send('Internal Server Error');
+  } finally {
+    // Close the MongoDB connection
+    await client.close();
+  }
+});
+
+// New endpoint to fetch data from MongoDB
+app.get("/data", async (req, res) => {
+  try {
+    // Connect to MongoDB
+    await client.connect();
+
+    const database = client.db('rescue'); // Replace with your database name
+    const collection = database.collection('person'); // Replace with your collection name
+
+    // Fetch data from MongoDB
+    const data = await collection.find({}).toArray();
+
+    // Send the fetched data to the client
+    res.json(data);
+  } catch (error) {
+    console.error('Error fetching data from MongoDB:', error);
+    res.status(500).send('Internal Server Error');
+  } finally {
+    // Close the MongoDB connection
+    await client.close();
+  }
+});
 
 const server = app.listen(port, () => {
   console.log(`Server is running on ${port}`);
 });
-
-const io = require("socket.io")(server, {
-  cors: {
-    origin: "http://localhost:3001",
-    methods: ["GET", "POST"],
-  },
-});
-
-io.on("connection", (socket) => {
-  console.log("a user connected");
-
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
-});
-
-// configuring open ai
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
-});
-
-
-const openai = new OpenAIApi(configuration);
-
-
-app.post("/", (req, res) => {
-    // Handle the POST request data
-    const postData = req.body;
-  
-    // Add your logic to process the received data
-    console.log("Received POST request:", postData);
-  
-    // Send a response back to the client
-    res.send("POST request received successfully");
-  });
